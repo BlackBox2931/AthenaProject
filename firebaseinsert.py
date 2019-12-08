ad8c5e0f9df7fffe622b5f9a34b585ecff8860b1
BlackBox2931 <pttrinh@cpp.edu>
2019-12-07 18:15:31 -0800

    changed firebaseinsert.py

--- firebaseinsert.py
+++ firebaseinsert.py
@@ -1,1 +1,12 @@
-asdfas
+from firebase import firebase
+
+
+firebase = firebase.FirebaseApplication("https://athena-s-traffic.firebaseio.com/", None)
+data = {
+    'Name':'Parwiz Forogh',
+    'Email':'par@gmail.com',
+    'Phone':7149110000
+
+}
+
+result = firebase.post()
